b31e5388a12731ef8a0d273b006d3420372be22e
NettleHook <catharina.castillo@gmail.com>
2026-04-13 21:49:55 -0700

    fix: Didn't index patient search results correctly, now fixed

--- pages/new_patient.py
+++ pages/new_patient.py
@@ -22,16 +22,12 @@ with st.form("patient_add_form"):
     patient_medications = st.text_input("Please enter patient's known medications as a comma separated list", placeholder="medications")
     patient_allergies = st.text_input("Please enter patient's known allergies as a comma separated list", placeholder="allergies")
     patient_notes = st.text_input("Please enter any additional notes about patient's basic medical history (optional)", placeholder="notes")
-    if 'Stash_SOAP' in st.session_state:
-        patient_soap = st.text_input("Please enter visit documentation (optional)", value = st.session_state['Stash_SOAP'])
-    else:
-        patient_soap = st.text_input("Please enter visit documentation (optional)", value = "")
     submitted = st.form_submit_button("Upload Patient Info")
     
 if submitted:
     patient_history = {"conditions": patient_conditions.split(",") if patient_conditions else [], "medications": patient_medications.split(",") if patient_medications else [], "allergies": patient_allergies.split(",") if patient_allergies else [], "notes":patient_notes}
     try:
-        response = add_patient(patient_first_name, patient_last_name, patient_dob, patient_sex, patient_history, patient_age, patient_soap)
+        response = add_patient(patient_first_name, patient_last_name, patient_dob, patient_sex, patient_history, patient_age)
         if response.status_code in (200, 201, 204):
             st.write("SOAP record added successfully.")
             #Reload patient data
